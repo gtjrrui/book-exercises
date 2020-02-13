25924e16e971cb8922baec286a24f8d670287979
gtjrrui <gtjrrui@uw.edu>
2020-02-13 14:46:48 -0800

    Week 6 Exercise Commit

--- chapter-16-exercises/exercise-2/exercise.R
+++ chapter-16-exercises/exercise-2/exercise.R
@@ -13,52 +13,63 @@ library("ggplot2")
 # Draw a column (bar) chart of diamonds cuts by price, with each bar filled by 
 # clarity. You should see a _stacked_ bar chart.
 
-
+ggplot(data = diamonds) + geom_col(mapping = aes(x = cut, y = price, fill = clarity))
+                                   
 # Draw the same chart again, but with each element positioned to "fill" the y axis
 
+ggplot(data = diamonds) + geom_col(mapping = aes(x = cut, y = price, fill = clarity), position = "fill")
 
 # Draw the same chart again, but with each element positioned to "dodge" each other
 
+ggplot(data = diamonds) + geom_col(mapping = aes(x = cut, y = price, fill = clarity), position = "dodge")
 
 # Draw a plot with point geometry with the x-position mapped to `cut` and the 
 # y-position mapped to `clarity`
 # This creates a "grid" grouping the points
 
+ggplot(data = diamonds) + geom_point(mapping = aes(x = cut, y = clarity))
 
 # Use the "jitter" position adjustment to keep the points from all overlapping!
 # (This works a little better with a sample of diamond data, such as from the 
 # previous exercise).
 
-
+ggplot(data = diamonds) + geom_point(mapping = aes(x = cut, y = clarity), position = "jitter")
 
 ## Scales
 
 # Draw a "boxplot" (with `geom_boxplot`) for the diamond's price (y) by color (x)
 
+ggplot(data = diamonds) + geom_boxplot(mapping = aes(x = color, y = price))
 
 # This has a lot of outliers, making it harder to read. To fix this, draw the 
 # same plot but with a _logarithmic_ scale for the y axis.
 
+ggplot(data = diamonds) + geom_boxplot(mapping = aes(x = color, y = price)) + scale_y_log10()
 
 # For another version, draw the same plot but with `violin` geometry instead of 
 # `boxplot` geometry!
 # How does the logarithmic scale change the data presentation?
 
+ggplot(data = diamonds) + geom_violin(mapping = aes(x = color, y = price)) + scale_y_log10()
+# Outliers are removed
 
 # Another interesting plot: draw a plot of the diamonds price (y) by carat (x), 
 # using a heatmap of 2d bins (geom_bin2d)
 # What happens when you make the x and y channels scale logarithmically?
 
+ggplot(data = diamonds) + geom_violin(mapping = aes(x = carat, y = price)) + scale_y_log10() + scale_x_log10()
+# Outliers are removed to find average price per carat
 
 # Draw a scatter plot for the diamonds price (y) by carat (x). Color each point
 # by the clarity (Remember, this will take a while. Use a sample of the diamonds 
 # for faster results)
 
+ggplot(data = diamonds) + geom_violin(mapping = aes(x = carat, y = price, color = "clarity"))
 
 # Change the color of the previous plot using a ColorBrewer scale of your choice. 
 # What looks nice?
 
-
+ggplot(data = diamonds) + geom_violin(mapping = aes(x = carat, y = price, color = "clarity")) + scale_color_brewer(palette = "Spectral")
 
 ## Coordinate Systems
 
@@ -67,20 +78,25 @@ library("ggplot2")
 # between)
 # TIP: You can save the plot to a variable for easier modifications
 
+chart <- ggplot(data = diamonds) + geom_bar(mapping = aes(x = cut, fill = cut), width = 1)
+chart
 
 # Draw the same chart, but with the coordinate system flipped
 
+flipped_chart <- chart + coord_flip()
+flipped_chart
 
 # Draw the same chart, but in a polar coordinate system. It's a Coxcomb chart!
 
-
+polar_chart <- chart + coord_polar()
+polar_chart
 
 ## Facets
 
 # Take the scatter plot of price by carat data (colored by clarity) and add 
 # _facets_ based on the diamond's `color`
 
-
+ggplot(data = diamonds) + geom_point(mapping = aes(x = carat, y = price, color = clarity)) + scale_color_brewer(palette = "Spectral") + facet_wrap(~ color)
 
 ## Saving Plots
 
@@ -88,3 +104,4 @@ library("ggplot2")
 # Name the output file "my-plot.png".
 # Make sure you've set the working directory!!
 
+#ggsave("my-plot.png")
--- chapter-18-exercises/exercise-1/exercise.R
+++ chapter-18-exercises/exercise-1/exercise.R
@@ -7,10 +7,12 @@ library("jsonlite")
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
-
+setwd("~/a5-data-report-gtjrrui")
+source("apikey.R")
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
 
+movie_name <- "Letters From Iwo Jima"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -21,25 +23,44 @@ library("jsonlite")
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie_name` variable as the search query!
 
+base_uri <- "https://api.nytimes.com/svc/movies/v2"
+resource <- "/reviews/search.json"
+query_params <- list("api-key" = nyt_apikey, query = movie_name)
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
 
+request <- GET(paste0(base_uri, resource), query = query_params)
+nytcontent <- content(request, "text")
+extract <- fromJSON(nytcontent)
 
 # What kind of data structure did this produce? A data frame? A list?
 
+is.data.frame(extract)
+is.list(extract)
+
+# List
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
 
+names(body)
+names(body$results)
 
 # Flatten the movie reviews content into a data structure called `reviews`
 
+reviews <- flatten(body$results)
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
 
+review <- reviews[1, ]
+headline <- review$headline
+summary_short <- review$summary_short
+link <- review$link.url
 
 # Create a list of the three pieces of information from above. 
 # Print out the list.
+
+info <- list("Headline" = headline, "Summary" = summary_short, "Link" = link)
